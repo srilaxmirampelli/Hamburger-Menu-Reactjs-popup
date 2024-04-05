92dfa05646635d1929de20ee2cb95075602410d0
srilaxmirampelli <srilaxmirampelli1010@gmail.com>
2024-04-05 08:06:18 +0000

    Hamburger menu built using Reactjs-popup

---
+++ src/components/About/index.js
@@ -0,0 +1,20 @@
+import './index.css'
+
+const About = () => (
+  <>
+    <div className="home-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/about-sm-img.png"
+        alt="about"
+        className="mobile-home-image"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/about-lg-img.png"
+        alt="about"
+        className="desktop-home-image"
+      />
+    </div>
+  </>
+)
+
+export default About
---
+++ src/components/Home/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+const Home = () => (
+  <div className="home-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-sm-img.png"
+      alt="home"
+      className="mobile-home-image"
+    />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png"
+      alt="home"
+      className="desktop-home-image"
+    />
+  </div>
+)
+
+export default Home
